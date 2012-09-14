8aff3c7277693201260693834e5b4da922d1bf0c
Dr Nic Williams <drnicwilliams@gmail.com>
2012-09-14 11:47:17 -0700

    staff vs normal users

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -2,6 +2,9 @@ Given /^I am a (normal|staff) user "(.*?)"$/ do |type, name|
   unless user = User.find_by_name(name)
     raise "Unknown User with name '#{name}'"
   end
+  if type == "staff" && !user.staff?
+    raise "User '#{name}' is not a staff user"
+  end
   visit "/users/sign_in"
   fill_in "Email", with: user.email
   fill_in "Password", with: "abc123"
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120914174453) do
+ActiveRecord::Schema.define(:version => 20120914184123) do
 
   create_table "users", :force => true do |t|
-    t.string   "name",                   :default => "", :null => false
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "name",                   :default => "",    :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -25,8 +25,9 @@ ActiveRecord::Schema.define(:version => 20120914174453) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.boolean  "staff",                  :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
