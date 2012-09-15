dd879232953d6dd029e1e2ec0e51dc3c66427362
Dr Nic Williams <drnicwilliams@gmail.com>
2012-09-15 13:45:16 -0700

    _header is run in main app, so reference partial via "impersonate_engine"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 UserImpersonate::Engine.routes.draw do
   match("/user/:user_id" , to: "impersonate#create", as: :impersonate_user)
+  match("/revert" , to: "impersonate#delete", as: :revert_impersonate_user)
   
   root to: "impersonate#index"
 end
