273f2dea8a2d8df31c026be592b26bdc3ac33899
Dr Nic Williams <drnicwilliams@gmail.com>
2012-09-15 13:48:35 -0700

    remove unnecessary file

--- lib/user_impersonate/engine.rb
+++ lib/user_impersonate/engine.rb
@@ -2,13 +2,6 @@ module UserImpersonate
   class Engine < ::Rails::Engine
     isolate_namespace UserImpersonate
     
-    # initializer "user_impersonate.rack_session_ext" do |app|
-    # require 'rack/session/abstract/id'
-    #   Rack::Session::Abstract::SessionHash.class_eval do
-    #     include UserImpersonate::RackSessionExt
-    #   end
-    # end
-
     initializer "user_impersonate.devise_helpers" do
       if Object.const_defined?("Devise")
         require "user_impersonate/devise_helpers"
