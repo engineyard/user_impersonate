36e1bb0fed9016525e2efcfee33f2b5bfc75f151
Dr Nic Williams <drnicwilliams@gmail.com>
2012-09-15 13:49:40 -0700

    remove debug/trace code

--- lib/user_impersonate/devise_helpers.rb
+++ lib/user_impersonate/devise_helpers.rb
@@ -3,19 +3,14 @@ module UserImpersonate
   module DeviseHelpers
     module Helpers
       def impersonate(new_user)
-        puts "impersonate(#{new_user.inspect})"
         session[:staff_user_id] = current_user.id # 
         sign_in new_user, bypass: true
-        # warden.set_user(staff_user, scope: "staff_user")
-        # warden.set_user(new_user)
-        p session
       end
     end
     
     module UrlHelpers
       def staff_user
         return unless session[:staff_user_id]
-        p session
         @staff_user ||= User.find(session[:staff_user_id])
       end
     end
