0a05690eadbc383899f720f15120fc29c51153e3
Dr Nic Williams <drnicwilliams@gmail.com>
2012-09-19 09:42:57 -0700

    Default configuration preloaded

--- lib/user_impersonate/engine.rb
+++ lib/user_impersonate/engine.rb
@@ -8,5 +8,13 @@ module UserImpersonate
         Devise.include_helpers(UserImpersonate::DeviseHelpers)
       end
     end
+
+    # also duplicated in generator initializer
+    initializer "user_impersonate.default_config" do
+      config.user_class           = "User"
+      config.user_finder          = "find"   # User.find
+      config.user_id_column       = "id"     # Such that User.find(aUser.id) works
+      config.user_is_staff_method = "staff?" # current_user.staff?
+    end
   end
 end
